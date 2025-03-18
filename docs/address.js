9b9d73e5569df9eee50c236aeafda38e9a239200
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-03-19 09:17:30 +1100

    address

--- docs/address.js
+++ docs/address.js
@@ -23,37 +23,40 @@ const Address = {
 
           <b-card no-body no-header bg-variant="light" class="m-1 p-1 w-75">
 
-            {{ address }}
-
             <b-form-group label-cols-lg="2" label="Address" label-size="md" label-class="font-weight-bold pt-0" class="mt-3 mb-0">
-              <b-form-group label="Address:" label-for="address-address" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+              <b-form-group v-if="error" label="Error:" label-for="address-error" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+                <b-form-input type="text" plaintext size="sm" id="address-error" :value="error"></b-form-input>
+              </b-form-group>
+
+              <b-form-group v-if="!error" label="Address:" label-for="address-address" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
                 <b-input-group>
-                  <b-button v-if="block && block.number != null" :href="'https://etherscan.io/block/' + block.number" variant="link" target="_blank" class="m-0 p-0 pt-1">
-                    {{ commify0(block.number) }}
-                  </b-button>
+                  <b-form-input type="text" plaintext size="sm" :value="address"></b-form-input>
+                  <!-- <b-button v-if="address" :href="'#/address/' + address" variant="link" class="m-0 p-0 pt-1">
+                    {{ address }}
+                  </b-button> -->
                   <b-input-group-append>
-                    <!-- <b-button v-if="block && block.number != null" size="sm" @click="loadAddress(parseInt(blockNumber) + 1);" variant="link">
-                      <b-icon-chevron-right shift-v="-1" font-scale="1.1"></b-icon-chevron-right>
-                    </b-button> -->
-                    <b-button v-if="block && block.number != null" size="sm" @click="copyToClipboard(block.number);" variant="link">
+                    <b-button v-if="address" :href="'https://etherscan.io/address/' + address" variant="link" target="_blank" class="m-0 ml-2 p-0 pt-1">
+                      <b-icon-link-45deg shift-v="-1" font-scale="1.1"></b-icon-link-45deg>
+                    </b-button>
+                    <b-button v-if="address" size="sm" @click="copyToClipboard(address);" variant="link">
                       <b-icon-clipboard shift-v="-1" font-scale="1.1"></b-icon-clipboard>
                     </b-button>
                   </b-input-group-append>
                 </b-input-group>
               </b-form-group>
 
-              <!-- <b-form-group label="Timestamp:" label-for="address-timestamp" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
-                <b-form-input type="text" plaintext size="sm" id="address-timestamp" :value="block && block.timestamp && formatTimestamp(block.timestamp) || ''"></b-form-input>
-              </b-form-group> -->
+              <b-form-group v-if="!error" label="Transaction Count:" label-for="address-transactioncount" label-size="sm" label-cols-sm="3" label-align-sm="right" class="mx-0 my-1 p-0">
+                <b-form-input type="text" plaintext size="sm" id="address-transactioncount" :value="commify0(transactionCount) || ''"></b-form-input>
+              </b-form-group>
 
             </b-form-group>
           </b-card>
-          <b-card-text>
+          <!-- <b-card-text>
             <h5>Address</h5>
             error: {{ error }}
             <br />
-            block: {{ block }}
-          </b-card-text>
+            address: {{ address }}
+          </b-card-text> -->
         </b-card>
       </b-card>
     </div>
@@ -67,21 +70,13 @@ const Address = {
   },
   computed: {
     error() {
-      return store.getters['block/error'];
+      return store.getters['address/error'];
     },
     address() {
       return store.getters['address/address'];
     },
-    block() {
-      return store.getters['block/block'];
-    },
-    extraDataAsString() {
-      try {
-        const extraData = store.getters['block/block'] && store.getters['block/block'].extraData || null;
-        return extraData && ethers.utils.toUtf8String(extraData) || null;
-      } catch (e) {
-      }
-      return null;
+    transactionCount() {
+      return store.getters['address/transactionCount'];
     },
     transactions() {
       const results = [];
@@ -148,34 +143,25 @@ const addressModule = {
   state: {
     error: null,
     address: null,
-    // transactionCount: null,
+    transactionCount: null,
     // ensName: null,
     // transactions: [],
-
-    // TODO: Delete
-    block: null,
   },
   getters: {
     error: state => state.error,
     address: state => state.address,
-    // transactionCount: state => state.transactionCount,
+    transactionCount: state => state.transactionCount,
     // ensName: state => state.ensName,
     // transactions: state => state.transactions,
-
-    // TODO: Delete
-    block: state => state.block,
   },
   mutations: {
     setData(state, data) {
       console.log(now() + " addressModule - mutations.setData - data: " + JSON.stringify(data));
       state.error = data.error;
       state.address = data.address;
-      // state.transactionCount = data.transactionCount;
+      state.transactionCount = data.transactionCount;
       // state.ensName = data.ensName;
       // state.transactions = data.transactions;
-
-      // TODO: Delete
-      state.block = data.block;
     },
   },
   actions: {
@@ -187,7 +173,7 @@ const addressModule = {
           error = "Not connected";
         }
         if (!error && !(/^0x([A-Fa-f0-9]{40})$/.test(nameOrAddress))) {
-          error = "Invalid address (names will be included later)";
+          error = "Invalid address (ENS names will be supported later)";
         }
         if (!error) {
           try {
@@ -196,61 +182,14 @@ const addressModule = {
             error = "Invalid address: " + nameOrAddress;
           }
         }
-      //   if (!error) {
-      //     const provider = new ethers.providers.Web3Provider(window.ethereum);
-      //     // const tx_ = await provider.getTransaction(txHash);
-      //     // console.log("tx_: " + JSON.stringify(tx_, null, 2));
-      //     // tx = {
-      //     //   hash: tx_.hash,
-      //     //   chainId: parseInt(tx_.chainId),
-      //     //   blockNumber: parseInt(tx_.blockNumber),
-      //     //   transactionIndex: tx_.transactionIndex,
-      //     //   type: tx_.type || null,
-      //     //   from: tx_.from,
-      //     //   to: tx_.to,
-      //     //   nonce: parseInt(tx_.nonce),
-      //     //   value: ethers.BigNumber.from(tx_.value).toString(),
-      //     //   data: tx_.data,
-      //     //   gasLimit: parseInt(tx_.gasLimit),
-      //     //   gasPrice: ethers.BigNumber.from(tx_.gasPrice).toString(),
-      //     //   maxFeePerGas: tx_.maxFeePerGas && ethers.BigNumber.from(tx_.maxFeePerGas).toString() || null,
-      //     //   maxPriorityFeePerGas: tx_.maxPriorityFeePerGas && ethers.BigNumber.from(tx_.maxPriorityFeePerGas).toString() || null,
-      //     //   // accessList: tx_.accessList,
-      //     //   // blockHash: tx_.blockHash,
-      //     //   // r: tx_.r,
-      //     //   // s: tx_.s,
-      //     //   // v: tx_.v,
-      //     //   // creates: tx_.creates,
-      //     // };
-      //     // const txReceipt_ = await provider.getTransactionReceipt(txHash);
-      //     // console.log("txReceipt_: " + JSON.stringify(txReceipt_, null, 2));
-      //     // txReceipt = {
-      //     //   status: txReceipt_.status,
-      //     //   type: txReceipt_.type,
-      //     //   byzantium: txReceipt_.byzantium || null,
-      //     //   contractAddress: txReceipt_.contractAddress,
-      //     //   gasUsed: parseInt(txReceipt_.gasUsed),
-      //     //   cumulativeGasUsed: parseInt(txReceipt_.cumulativeGasUsed),
-      //     //   effectiveGasPrice: ethers.BigNumber.from(txReceipt_.effectiveGasPrice).toString(),
-      //     //   logs: txReceipt_.logs,
-      //     //   // to: txReceipt_.to,
-      //     //   // from: txReceipt_.from,
-      //     //   // blockNumber: parseInt(txReceipt_.blockNumber),
-      //     //   // logsBloom: txReceipt_.logsBloom,
-      //     //   // blockHash: txReceipt_.blockHash,
-      //     // };
-      //     block = await provider.getBlockWithTransactions(parseInt(blockNumber)) || null;
-      //     // console.log("DEBUG 4 - block: " + JSON.stringify(block));
-      //     // timestamp = block && block.timestamp || null;
-      //     // if (!tx || !txReceipt) {
-      //     //   error = "Address with specified number cannot be found"
-      //     // }
-      //   }
+        if (!error) {
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          transactionCount = await provider.getTransactionCount(address);
+          console.log(now() + " addressModule - actions.loadAddress - transactionCount: " + transactionCount);
+        }
       }
       console.log("error: " + error);
-      // console.log("block: " + JSON.string(block, null, 2));
-      // console.table(block);
-      context.commit('setData', { error, address });
+      context.commit('setData', { error, address, transactionCount });
     }
   },
 };
