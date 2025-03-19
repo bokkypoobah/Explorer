a32dd81d287becc468ae92d6fdfb0c0b61ea387c
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-03-19 13:11:08 +1100

    eth_getBlockByHash test

--- docs/block.js
+++ docs/block.js
@@ -273,58 +273,13 @@ const blockModule = {
         }
         if (!error) {
           const provider = new ethers.providers.Web3Provider(window.ethereum);
-          // const tx_ = await provider.getTransaction(txHash);
-          // console.log("tx_: " + JSON.stringify(tx_, null, 2));
-          // tx = {
-          //   hash: tx_.hash,
-          //   chainId: parseInt(tx_.chainId),
-          //   blockNumber: parseInt(tx_.blockNumber),
-          //   transactionIndex: tx_.transactionIndex,
-          //   type: tx_.type || null,
-          //   from: tx_.from,
-          //   to: tx_.to,
-          //   nonce: parseInt(tx_.nonce),
-          //   value: ethers.BigNumber.from(tx_.value).toString(),
-          //   data: tx_.data,
-          //   gasLimit: parseInt(tx_.gasLimit),
-          //   gasPrice: ethers.BigNumber.from(tx_.gasPrice).toString(),
-          //   maxFeePerGas: tx_.maxFeePerGas && ethers.BigNumber.from(tx_.maxFeePerGas).toString() || null,
-          //   maxPriorityFeePerGas: tx_.maxPriorityFeePerGas && ethers.BigNumber.from(tx_.maxPriorityFeePerGas).toString() || null,
-          //   // accessList: tx_.accessList,
-          //   // blockHash: tx_.blockHash,
-          //   // r: tx_.r,
-          //   // s: tx_.s,
-          //   // v: tx_.v,
-          //   // creates: tx_.creates,
-          // };
-          // const txReceipt_ = await provider.getTransactionReceipt(txHash);
-          // console.log("txReceipt_: " + JSON.stringify(txReceipt_, null, 2));
-          // txReceipt = {
-          //   status: txReceipt_.status,
-          //   type: txReceipt_.type,
-          //   byzantium: txReceipt_.byzantium || null,
-          //   contractAddress: txReceipt_.contractAddress,
-          //   gasUsed: parseInt(txReceipt_.gasUsed),
-          //   cumulativeGasUsed: parseInt(txReceipt_.cumulativeGasUsed),
-          //   effectiveGasPrice: ethers.BigNumber.from(txReceipt_.effectiveGasPrice).toString(),
-          //   logs: txReceipt_.logs,
-          //   // to: txReceipt_.to,
-          //   // from: txReceipt_.from,
-          //   // blockNumber: parseInt(txReceipt_.blockNumber),
-          //   // logsBloom: txReceipt_.logsBloom,
-          //   // blockHash: txReceipt_.blockHash,
-          // };
           block = await provider.getBlockWithTransactions(parseInt(blockNumber)) || null;
-          // console.log("DEBUG 4 - block: " + JSON.stringify(block));
-          // timestamp = block && block.timestamp || null;
-          // if (!tx || !txReceipt) {
-          //   error = "Block with specified number cannot be found"
-          // }
+          // console.log("blockNumber: " + blockNumber + ", blockHash: " + block.hash);
+          // const blockByBlockHash = await provider.send('eth_getBlockByHash', [block.hash, true]);
+          // console.log("blockByBlockHash.number: " + parseInt(blockByBlockHash.number) + ", blockByBlockHash.hash: " + blockByBlockHash.hash);
+          // console.log("blockByBlockHash: " + JSON.stringify(blockByBlockHash, null, 2));
         }
       }
-      // console.log("error: " + error);
-      // console.log("block: " + JSON.string(block, null, 2));
-      // console.table(block);
       context.commit('setData', { error, block });
     }
   },
