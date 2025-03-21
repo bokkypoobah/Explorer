6d53d6997b30a7e6c29a0ef6378370e605238f18
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-03-21 13:11:34 +1100

    Upgraded ethers.js to 5.8.0 and created an includes checking tool

--- checkIncludes/00_checkIt.sh
+++ checkIncludes/00_checkIt.sh
@@ -2,6 +2,7 @@
 
 RESULTS=results.txt
 date > $RESULTS
+echo "" >> $RESULTS
 
 # Check Dexie
 SOURCE=https://unpkg.com/dexie@3.0.3/dist/dexie.js
@@ -9,6 +10,8 @@ SOURCETEMP=dexie.js_3.0.3
 INSTALLED=../docs/js/dexie.js
 echo "Checking installed ${INSTALLED} vs ${SOURCE}" >> $RESULTS
 wget $SOURCE -O $SOURCETEMP
+echo "- shasum -a 256 ${INSTALLED} ${SOURCETEMP}" >> $RESULTS
+shasum -a 256 ${INSTALLED} ${SOURCETEMP} | sed 's/^/  - /' >> $RESULTS
 if diff $SOURCETEMP $INSTALLED >& /dev/null; then
     echo "- Installed ${INSTALLED} is identical to ${SOURCE}" >> $RESULTS
 else
@@ -16,6 +19,7 @@ else
     # diff $SOURCETEMP $INSTALLED >> $RESULTS
 fi
 rm $SOURCETEMP
+echo "" >> $RESULTS
 
 # Check moment.js
 SOURCE=https://unpkg.com/moment@2.24.0
@@ -23,6 +27,8 @@ SOURCETEMP=moment.js_2.24.0
 INSTALLED=../docs/js/moment.js
 echo "Checking installed ${INSTALLED} vs ${SOURCE}" >> $RESULTS
 wget $SOURCE -O $SOURCETEMP
+echo "- shasum -a 256 ${INSTALLED} ${SOURCETEMP}" >> $RESULTS
+shasum -a 256 ${INSTALLED} ${SOURCETEMP} | sed 's/^/  - /' >> $RESULTS
 if diff $SOURCETEMP $INSTALLED >& /dev/null; then
     echo "- Installed ${INSTALLED} is identical to ${SOURCE}" >> $RESULTS
 else
@@ -30,6 +36,7 @@ else
     # diff $SOURCETEMP $INSTALLED >> $RESULTS
 fi
 rm $SOURCETEMP
+echo "" >> $RESULTS
 
 # Check vuex.js
 SOURCE=https://unpkg.com/vuex@3.6.2/dist/vuex.js
@@ -37,6 +44,25 @@ SOURCETEMP=vuex.js_3.6.2
 INSTALLED=../docs/js/vuex.js
 echo "Checking installed ${INSTALLED} vs ${SOURCE}" >> $RESULTS
 wget $SOURCE -O $SOURCETEMP
+echo "- shasum -a 256 ${INSTALLED} ${SOURCETEMP}" >> $RESULTS
+shasum -a 256 ${INSTALLED} ${SOURCETEMP} | sed 's/^/  - /' >> $RESULTS
+if diff $SOURCETEMP $INSTALLED >& /dev/null; then
+    echo "- Installed ${INSTALLED} is identical to ${SOURCE}" >> $RESULTS
+else
+    echo "- Installed ${INSTALLED} differs from ${SOURCE}" >> $RESULTS
+    # diff $SOURCETEMP $INSTALLED >> $RESULTS
+fi
+rm $SOURCETEMP
+echo "" >> $RESULTS
+
+# Check ethers.js
+SOURCE=https://cdnjs.cloudflare.com/ajax/libs/ethers/5.8.0/ethers.umd.min.js
+SOURCETEMP=ethers-5.8.0.umd.min.js
+INSTALLED=../docs/js/ethers-5.8.0.umd.min.js
+echo "Checking installed ${INSTALLED} vs ${SOURCE}" >> $RESULTS
+wget $SOURCE -O $SOURCETEMP
+echo "- shasum -a 256 ${INSTALLED} ${SOURCETEMP}" >> $RESULTS
+shasum -a 256 ${INSTALLED} ${SOURCETEMP} | sed 's/^/  - /' >> $RESULTS
 if diff $SOURCETEMP $INSTALLED >& /dev/null; then
     echo "- Installed ${INSTALLED} is identical to ${SOURCE}" >> $RESULTS
 else
