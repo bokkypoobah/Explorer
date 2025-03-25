ee3175c6df44bd8b537f70f1517da05238f8fae1
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-03-25 14:31:04 +1100

    contract function wip

--- docs/samples.js
+++ docs/samples.js
@@ -1,4 +1,5 @@
 const SAMPLES = {
+  // ETH Mainnet
   "1": [
     { type: "contract", address: "0x9fC3dc011b461664c835F2527fffb1169b3C213e", info: "EF: DeFi Multisig - Safe v1.4.1" },
     { type: "contract", address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", info: "ERC-20: WETH" },
@@ -11,8 +12,29 @@ const SAMPLES = {
     { type: "contract", address: "0x78F96B2D5F717fa9ad416957B79D825cc4ccE69d", info: "BokkyPooBahsDateTimeLibrary:TestDateTime" },
     { type: "contract", address: "0x0eE1d7943890D8335110B879C25271e864Ee7CA8", info: "ERC721Helper" },
   ],
+  // Sepolia Testnet
   "11155111": [
     { type: "contract", address: "0x7439E9Bb6D8a84dd3A23fe621A30F95403F87fB9", info: "ERC-20: WEENUS" },
     { type: "contract", address: "0x07391dbE03e7a0DEa0fce6699500da081537B6c3", info: "ERC-20: WETH" },
+  ],
+  // Arbitrum One Mainnet
+  "42161": [
+    { type: "contract", address: "0x429106bAb5981ABb59094b9780cb731Cf1dC16Ad", info: "ERC-20: WEENUS" },
+    { type: "contract", address: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", info: "ERC-20: WETH" },
+  ],
+  // Base Mainnet
+  "8453": [
+    { type: "contract", address: "0x83377ea907a08baC9320cE6330993EcabBB48578", info: "ERC-20: WEENUS" },
+    { type: "contract", address: "0x4200000000000000000000000000000000000006", info: "ERC-20: WETH" },
+  ],
+  // Linea Mainnet
+  "59144": [
+    { type: "contract", address: "0x9C4B276d8C6CDa3C2E2E6401f4a502c1E15847E9", info: "ERC-20: WEENUS" },
+    { type: "contract", address: "0xe5D7C2a44FfDDf6b295A15c148167daaAf5Cf34f", info: "ERC-20: WETH" },
+  ],
+  // OP Mainnet
+  "10": [
+    { type: "contract", address: "0x07391dbE03e7a0DEa0fce6699500da081537B6c3", info: "ERC-20: WEENUS" },
+    { type: "contract", address: "0x4200000000000000000000000000000000000006", info: "ERC-20: WETH" },
   ],
 };
