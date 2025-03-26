7731eecda58ba1fcddc703638a5a9258abccde11
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-03-26 19:58:10 +1100

    web3 connection

---
+++ new_docs/connectionModule.js
@@ -0,0 +1,188 @@
+// ----------------------------------------------------------------------------
+// Web3 connection, including coinbase and coinbase balance
+// ----------------------------------------------------------------------------
+const Connection = {
+  template: `
+    <div>
+      <p class="text-caption text--disabled">
+        {{ connected ? "Connected" : "Disconnected" }}
+        &nbsp;
+        <a v-if="info.chainId" :href="explorer" target="_blank">
+          {{ networkName }}
+        </a>
+        &nbsp;
+        <a v-if="info.blockNumber" :href="explorer + 'block/' + info.blockNumber" target="_blank">
+          {{ '#' + commify0(info.blockNumber) }}
+        </a>
+        &nbsp;
+        <span v-if="info.timestamp">
+          {{ formatTimeDiff(info.timestamp) }}
+        </span>
+        <!-- <a v-if="info.chainId" :href="explorer" target="_blank">
+          {{ networkName }}
+        </a> -->
+        <!-- <b-link v-if="info.blockNumber" :href="'#/block/' + info.blockNumber" v-b-popover.hover.bottom="'Latest block'">
+          {{ '#' + commify0(info.blockNumber) }}
+        </b-link> -->
+        <!-- <span v-if="info.timestamp" v-b-popover.hover.bottom="formatTimestamp(info.timestamp)">
+          {{ formatTimeDiff(info.timestamp) }}
+        </span> -->
+      </p>
+    </div>
+  `,
+  data: function () {
+    return {
+      count: 0,
+    }
+  },
+  computed: {
+    connected() {
+      return store.getters['web3'].connected;
+    },
+    info() {
+      return store.getters['web3'];
+    },
+    supportedNetwork() {
+      return store.getters['supportedNetwork'];
+    },
+    networkName() {
+      return store.getters['networkName'];
+    },
+    explorer() {
+      return store.getters['explorer'];
+    },
+  },
+  methods: {
+    commify0(n) {
+      if (n != null) {
+        return Number(n).toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 });
+      }
+      return null;
+    },
+    formatTimestamp(ts) {
+      if (ts != null) {
+        // if (this.settings.reportingDateTime == 1) {
+        //   return moment.unix(ts).utc().format("YYYY-MM-DD HH:mm:ss");
+        // } else {
+          return moment.unix(ts).format("YYYY-MM-DD HH:mm:ss");
+        // }
+      }
+      return null;
+    },
+    formatTimeDiff(unixtime) {
+      if (unixtime) {
+        return moment.unix(unixtime).fromNow();
+      }
+      return null;
+    },
+
+  },
+  mounted() {
+    console.log(now() + " Connection - mounted");
+    // store.dispatch('connection/restore');
+  },
+  destroyed() {
+    console.log(now() + " Connection - destroyed");
+  },
+};
+
+
+const connectionModule = {
+  namespaced: true,
+  state: {
+    provider: null,
+  },
+  getters: {
+  },
+  mutations: {
+    setProvider(state, provider) {
+      state.provider = provider;
+    },
+  },
+  actions: {
+    async connect(context) {
+      let [provider, connected, error, chainId, blockNumber, timestamp, coinbase] = [null, false, null, null, null, null, null];
+      if (window.ethereum) {
+        if (!window.ethereum.isConnected() || !window.ethereum['isUnlocked']) {
+          try {
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            connected = window.ethereum.isConnected();
+          } catch (e) {
+            error = "Unable to access the browser's web3 accounts";
+          }
+        } else {
+          connected = true;
+        }
+        if (connected) {
+          provider = new ethers.providers.Web3Provider(window.ethereum);
+          const network = await provider.getNetwork();
+          chainId = parseInt(network.chainId);
+          const block = await provider.getBlock("latest");
+          blockNumber = block.number;
+          timestamp = block.timestamp;
+          const signer = provider.getSigner();
+          coinbase = await signer.getAddress();
+        }
+
+        if (connected) {
+          function handleChainChanged(_chainId) {
+            alert('Web3 network changed. This page will reload 5 seconds after you click OK.')
+            setTimeout(function() {
+                window.location.reload();
+              }, 5000);
+          }
+          if (!window.ethereum._events.chainChanged) {
+            window.ethereum.on('chainChanged', handleChainChanged);
+          }
+        }
+
+        if (connected) {
+          async function handleAccountsChanged(accounts) {
+            const signer = provider.getSigner();
+            const coinbase = await signer.getAddress();
+            store.dispatch('setWeb3Coinbase', coinbase);
+          }
+          if (!window.ethereum._events.accountsChanged) {
+            window.ethereum.on('accountsChanged', handleAccountsChanged);
+          }
+        }
+
+        if (connected) {
+          // TODO: ethers.js fires duplicated new block events sometimes
+          async function handleNewBlock(blockNumber) {
+            const lastBlockNumber = store.getters['web3'].blockNumber;
+            if (!lastBlockNumber || blockNumber > lastBlockNumber) {
+              const block = await provider.getBlock("latest");
+              if (block.number > lastBlockNumber) {
+                store.dispatch('setWeb3BlockInfo', { blockNumber: block.number, timestamp: block.timestamp });
+                console.log(now() + " connectionModule - actions.connect.handleNewBlock - blockNumber: " + block.number);
+              }
+            }
+          }
+          if (provider._events.length == 0) {
+            provider.on("block", handleNewBlock);
+          }
+        }
+
+      } else {
+        error = "This app requires a web3 enabled browser";
+      }
+      store.dispatch('setWeb3Info', { connected, error, chainId, blockNumber, timestamp, coinbase });
+      context.commit('setProvider', provider);
+    },
+    restore(context) {
+      // if (store.getters['web3'].connected) {
+      //   context.dispatch('connect');
+      // }
+    },
+    disconnect(context) {
+      if (store.getters['web3'].connected) {
+        if (context.state.provider) {
+          context.state.provider.removeAllListeners();
+          window.ethereum.removeAllListeners();
+        }
+        store.dispatch('setWeb3Connected', false);
+      }
+    },
+  },
+};
--- new_docs/index.js
+++ new_docs/index.js
@@ -1,3 +1,5 @@
+// Vue.component('connection', Connection);
+
 // Create the router
 const router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
@@ -45,30 +47,55 @@ const store = new Vuex.Store({
     config: state => state.config,
     web3: state => state.web3,
     db: state => state.db,
+    chainId(state) {
+      return state.web3.chainId;
+    },
+    supportedNetwork(state) {
+      const chain = state.web3.chainId && state.config.chains[state.web3.chainId] || null;
+      return !!chain;
+    },
+    networkName(state) {
+      const chain = state.web3.chainId && state.config.chains[state.web3.chainId] || null;
+      return chain && chain.name || "Unknown chainId: " + state.web3.chainId;
+    },
+    explorer(state) {
+      const chain = state.web3.chainId && state.config.chains[state.web3.chainId] || null;
+      return chain && chain.explorer || null;
+    },
   },
   mutations: {
     setConfig(state, config) {
       state.config = config;
     },
-    // setName(state, newName) {
-    //   state.name = newName;
-    // }
     setEtherscanAPIKey(state, apiKey) {
       state.config.etherscanAPIKey = apiKey;
     },
     addChain(state, chain) {
       if (!(chain.chainId in state.config.chains)) {
-        // Vue.set(state.config.chains, chain.chainId, {
-        //   name: chain.name,
-        //   explorer: chain.explorer,
-        //   api: chain.api,
-        // });
         state.config.chains[chain.chainId] = {
           name: chain.name,
           explorer: chain.explorer,
           api: chain.api,
         };
       }
+    },
+    setWeb3Info(state, info) {
+      state.web3.connected = info.connected;
+      state.web3.error = info.error;
+      state.web3.chainId = info.chainId;
+      state.web3.blockNumber = info.blockNumber;
+      state.web3.timestamp = info.timestamp;
+      state.web3.coinbase = info.coinbase;
+    },
+    setWeb3Connected(state, connected) {
+      state.web3.connected = connected;
+    },
+    setWeb3Coinbase(state, coinbase) {
+      state.web3.coinbase = coinbase;
+    },
+    setWeb3BlockInfo(state, blockInfo) {
+      state.web3.blockNumber = blockInfo.blockNumber;
+      state.web3.timestamp = blockInfo.timestamp;
     },
   },
   actions: {
@@ -84,14 +111,29 @@ const store = new Vuex.Store({
     addChain(context, chain) {
       context.commit('addChain', chain);
     },
+    setWeb3Info(context, info) {
+      context.commit('setWeb3Info', info);
+    },
+    setWeb3Connected(context, connected) {
+      context.commit('setWeb3Connected', connected);
+    },
+    setWeb3Coinbase(context, coinbase) {
+      context.commit('setWeb3Coinbase', coinbase);
+    },
+    setWeb3BlockInfo(context, blockInfo) {
+      context.commit('setWeb3BlockInfo', blockInfo);
+    },
+  },
+  modules: {
+    connection: connectionModule,
   },
   plugins: [
     function persistSettings(store) {
       store.subscribe((mutation, state) => {
         if (mutation.type == "setEtherscanAPIKey" || mutation.type == "addChain") {
           localStorage.new_explorerConfig = JSON.stringify(state.config);
-        // } else if (mutation.type.substring(0, 7) == "setWeb3") {
-        //   localStorage.new_explorerWeb3 = JSON.stringify(state.web3Connection);
+        } else if (mutation.type.substring(0, 7) == "setWeb3") {
+          localStorage.new_explorerWeb3 = JSON.stringify(state.web3);
         }
       });
 
@@ -99,6 +141,12 @@ const store = new Vuex.Store({
         const tempConfig = JSON.parse(localStorage.new_explorerConfig);
         if ('version' in tempConfig && tempConfig.version == store.getters["config"].version) {
           store.dispatch('setConfig', tempConfig);
+        }
+      }
+      if (localStorage.new_explorerWeb3) {
+        const tempWeb3Info = JSON.parse(localStorage.new_explorerWeb3);
+        if ('version' in tempWeb3Info && tempWeb3Info.version == store.getters["web3"].version) {
+          store.dispatch('setWeb3Info', tempWeb3Info);
         }
       }
     },
@@ -125,11 +173,20 @@ const app = Vue.createApp({
   //   }
   // },
   computed: {
-    router() {
-      return this.$router;
+    connected() {
+      return store.getters['web3'].connected;
     },
+    // router() {
+    //   return this.$router;
+    // },
   },
-  // methods: {
+  methods: {
+    connect(connected) {
+      store.dispatch('connection/connect');
+    },
+    disconnect(connected) {
+      store.dispatch('connection/disconnect');
+    },
   //   testClick() {
   //     console.log('this is a test click from component', this.$store.state.test);
   //     this.$store.dispatch('pretendUpdate');
@@ -137,7 +194,7 @@ const app = Vue.createApp({
   //   submitClick() {
   //     console.log('Name in store', this.$store.state.name);
   //   }
-  // },
+  },
   beforeCreate() {
     console.log(now() + " index.js - app:beforeCreate");
 	},
@@ -156,5 +213,5 @@ const app = Vue.createApp({
 app.use(router);
 app.use(store);
 app.use(vuetify);
-// app.component("home", Home);
+app.component("connection", Connection);
 app.mount('#app');
