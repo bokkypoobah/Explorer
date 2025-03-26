0ae77e165d749f8d540aba8b4342fe119f16defa
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-03-26 16:47:29 +1100

    config

--- new_docs/config.js
+++ new_docs/config.js
@@ -1,13 +1,56 @@
 const Config = {
-  template: "<div><h1>Config</h1><p>TODO</p></div>",
+  template: `
+    <div>
+      <v-form ref="formConfig">
+        <!-- <h2>Config</h2> -->
+        <br />
+        <v-text-field v-model="etherscanAPIKey" label="Etherscan API Key:"></v-text-field>
+        <br />
+        <v-data-table :items="chains"></v-data-table>
+        <v-btn @click="importChainlistFromEtherscan();" text>Import from Etherscan</v-btn>
+      </v-form>
+    </div>
+  `,
   data: function () {
-    return {};
+    return {
+    };
   },
   computed: {
-
+    etherscanAPIKey: {
+      get: function() {
+        return store.getters['config'].etherscanAPIKey;
+      },
+      set: function(etherscanAPIKey) {
+        store.dispatch('setEtherscanAPIKey', etherscanAPIKey);
+      },
+    },
+    chains() {
+      const results = [];
+      for (const [chainId, chainData] of Object.entries(store.getters['config'].chains)) {
+        results.push({ chainId, ...chainData });
+      }
+      return results;
+    },
   },
   methods: {
-
+    async importChainlistFromEtherscan() {
+      console.log(now() + " Config - methods.importChainlistFromEtherscan()");
+      const url = "https://api.etherscan.io/v2/chainlist";
+      const data = await fetch(url).then(response => response.json());
+      console.log(JSON.stringify(data));
+      if (data && data.result) {
+        for (const item of data.result) {
+          if (!(item.chainid in store.getters['config'].chains)) {
+            store.dispatch('addChain', {
+              chainId: item.chainid,
+              name: item.chainname,
+              explorer: item.blockexplorer + (item.blockexplorer.substr(-1) != "/" ? "/" : ""),
+              api: item.apiurl,
+            });
+          }
+        }
+      }
+    },
   },
   beforeCreate() {
     console.log(now() + " Config - app:beforeCreate");
--- new_docs/index.js
+++ new_docs/index.js
@@ -6,19 +6,103 @@ const router = VueRouter.createRouter({
 
 const store = new Vuex.Store({
   state: {
-    testData: 'Hellooo',
-    name: 'Test'
+    config: {
+      etherscanAPIKey: null,
+      chains: {
+        1: {
+          name: "Ethereum Mainnet",
+          explorer: "https://etherscan.io/",
+          api: "https://api.etherscan.io/v2/api?chainid=1",
+        },
+        11155111: {
+          name: "Sepolia Testnet",
+          explorer: "https://sepolia.etherscan.io/",
+          api: "https://api.etherscan.io/v2/api?chainid=11155111",
+        },
+      },
+      version: 1,
+    },
+    web3: {
+      connected: false,
+      error: false,
+      chainId: null,
+      blockNumber: null,
+      timestamp: null,
+      coinbase: null,
+      version: 0,
+    },
+    db: {
+      name: "new_explorer088a",
+      version: 1,
+      schemaDefinition: {
+        cache: '&objectName',
+      },
+    },
+    // testData: 'Hellooo',
+    // name: 'Test'
+  },
+  getters: {
+    config: state => state.config,
+    web3: state => state.web3,
+    db: state => state.db,
   },
   mutations: {
-    setName(state, newName) {
-      state.name = newName;
-    }
+    setConfig(state, config) {
+      state.config = config;
+    },
+    // setName(state, newName) {
+    //   state.name = newName;
+    // }
+    setEtherscanAPIKey(state, apiKey) {
+      state.config.etherscanAPIKey = apiKey;
+    },
+    addChain(state, chain) {
+      if (!(chain.chainId in state.config.chains)) {
+        // Vue.set(state.config.chains, chain.chainId, {
+        //   name: chain.name,
+        //   explorer: chain.explorer,
+        //   api: chain.api,
+        // });
+        state.config.chains[chain.chainId] = {
+          name: chain.name,
+          explorer: chain.explorer,
+          api: chain.api,
+        };
+      }
+    },
   },
   actions: {
-    pretendUpdate({ commit }) {
-      commit('setName', 'Updated Name');
-    }
-  }
+    // pretendUpdate({ commit }) {
+    //   commit('setName', 'Updated Name');
+    // }
+    setConfig(context, config) {
+      context.commit('setConfig', config);
+    },
+    setEtherscanAPIKey(context, apiKey) {
+      context.commit('setEtherscanAPIKey', apiKey);
+    },
+    addChain(context, chain) {
+      context.commit('addChain', chain);
+    },
+  },
+  plugins: [
+    function persistSettings(store) {
+      store.subscribe((mutation, state) => {
+        if (mutation.type == "setEtherscanAPIKey" || mutation.type == "addChain") {
+          localStorage.new_explorerConfig = JSON.stringify(state.config);
+        // } else if (mutation.type.substring(0, 7) == "setWeb3") {
+        //   localStorage.new_explorerWeb3 = JSON.stringify(state.web3Connection);
+        }
+      });
+
+      if (localStorage.new_explorerConfig) {
+        const tempConfig = JSON.parse(localStorage.new_explorerConfig);
+        if ('version' in tempConfig && tempConfig.version == store.getters["config"].version) {
+          store.dispatch('setConfig', tempConfig);
+        }
+      }
+    },
+  ],
 });
 
 // console.table(Vuetify);
@@ -28,6 +112,9 @@ const vuetify = Vuetify.createVuetify({
 });
 
 const app = Vue.createApp({
+  // icons: {
+  //   iconfont: 'mdiSvg', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+  // },
   // router,
   // store,
   // vuetify,
