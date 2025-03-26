fb4085ae8a27090cab818bb9e2bb5f9fc83ba4a7
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-03-26 14:09:14 +1100

    tidy

--- vuetifyGetIncludes/00_getVuetifyIncludes.sh
+++ vuetifyGetIncludes/00_getVuetifyIncludes.sh
@@ -10,35 +10,34 @@ SOURCE=https://cdn.jsdelivr.net/npm/vue@3.5.13/dist/vue.global.prod.js
 DEST=../new_docs/js/vue_3.5.13.prod.js
 wget $SOURCE -O $DEST
 
-# # Get vuex[.min].js 4.1.0
-# SOURCE=https://cdn.jsdelivr.net/npm/vuex@4.1.0/dist/vuex.global.js
-# DEST=../new_docs/js/vuex_4.1.0.js
-# wget $SOURCE -O $DEST
-# SOURCE=https://cdn.jsdelivr.net/npm/vuex@4.1.0/dist/vuex.global.min.js
-# DEST=../new_docs/js/vuex_4.1.0.min.js
-# wget $SOURCE -O $DEST
+# Get vuex[.min].js 4.1.0
+SOURCE=https://cdn.jsdelivr.net/npm/vuex@4.1.0/dist/vuex.global.js
+DEST=../new_docs/js/vuex_4.1.0.js
+wget $SOURCE -O $DEST
+SOURCE=https://cdn.jsdelivr.net/npm/vuex@4.1.0/dist/vuex.global.min.js
+DEST=../new_docs/js/vuex_4.1.0.min.js
+wget $SOURCE -O $DEST
 
-# # Get vue-router[.min].js 4.5.0
-# SOURCE=https://cdn.jsdelivr.net/npm/vue-router@4.5.0/dist/vue-router.global.js
-# DEST=../new_docs/js/vue-router_4.5.0.js
-# wget $SOURCE -O $DEST
-# SOURCE=https://cdn.jsdelivr.net/npm/vue-router@4.5.0/dist/vue-router.global.min.js
-# DEST=../new_docs/js/vuex_4.5.0.min.js
-# wget $SOURCE -O $DEST
+# Get vue-router[.min].js 4.5.0
+SOURCE=https://cdn.jsdelivr.net/npm/vue-router@4.5.0/dist/vue-router.global.js
+DEST=../new_docs/js/vue-router_4.5.0.js
+wget $SOURCE -O $DEST
+SOURCE=https://cdn.jsdelivr.net/npm/vue-router@4.5.0/dist/vue-router.global.min.js
+DEST=../new_docs/js/vuex_4.5.0.min.js
+wget $SOURCE -O $DEST
 
-# # Get vuetify[.min].css 3.7.18
-# SOURCE=https://cdn.jsdelivr.net/npm/vuetify@3.7.18/dist/vuetify.css
-# DEST=../new_docs/css/vuetify_3.7.18.css
-# wget $SOURCE -O $DEST
-# SOURCE=https://cdn.jsdelivr.net/npm/vuetify@3.7.18/dist/vuetify.min.css
-# DEST=../new_docs/css/vuetify_3.7.18.min.css
-# wget $SOURCE -O $DEST
-#
-# # Get vuetify[.min].js 3.7.18
-# SOURCE=https://cdn.jsdelivr.net/npm/vuetify@3.7.18/dist/vuetify.js
-# DEST=../new_docs/js/vuetify_3.7.18.js
-# wget $SOURCE -O $DEST
-# SOURCE=https://cdn.jsdelivr.net/npm/vuetify@3.7.18/dist/vuetify.min.js
-# DEST=../new_docs/js/vuetify_3.7.18.min.js
-# wget $SOURCE -O $DEST
-#
+# Get vuetify[.min].css 3.7.18
+SOURCE=https://cdn.jsdelivr.net/npm/vuetify@3.7.18/dist/vuetify.css
+DEST=../new_docs/css/vuetify_3.7.18.css
+wget $SOURCE -O $DEST
+SOURCE=https://cdn.jsdelivr.net/npm/vuetify@3.7.18/dist/vuetify.min.css
+DEST=../new_docs/css/vuetify_3.7.18.min.css
+wget $SOURCE -O $DEST
+
+# Get vuetify[.min].js 3.7.18
+SOURCE=https://cdn.jsdelivr.net/npm/vuetify@3.7.18/dist/vuetify.js
+DEST=../new_docs/js/vuetify_3.7.18.js
+wget $SOURCE -O $DEST
+SOURCE=https://cdn.jsdelivr.net/npm/vuetify@3.7.18/dist/vuetify.min.js
+DEST=../new_docs/js/vuetify_3.7.18.min.js
+wget $SOURCE -O $DEST
