0bc9cb8925629ece3c4823590d0c9c4ec80ed2ba
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-03-30 00:56:27 +1100

    address contract wip

--- docs/addressContract.js
+++ docs/addressContract.js
@@ -7,6 +7,7 @@ const AddressContract = {
             Externally Owned Accounts don't have contract functions
           </div>
           <div v-if="info && info.type != 'eoa'">
+            <!-- isValid: {{ isValid }} -->
             <v-row>
               <v-col cols="2">
               <v-tabs v-model="settings.tab" @update:modelValue="saveSettings();" color="primary" direction="vertical">
@@ -19,48 +20,56 @@ const AddressContract = {
                 </v-select>
                 <v-card title="Inputs">
                   <v-card-text>
-                    <div v-if="selectedFunctionInputs.length == 0">
-                      No inputs
-                    </div>
-                    <v-row v-for="(item, inputIndex) of selectedFunctionInputs" no-gutters dense>
-                      <v-col>
-                        {{ inputIndex + 1}}
-                      </v-col>
-                      <v-col cols="11">
-                        <div v-if="item.arrayLength == null">
-                          <v-text-field :model-value="getInput(inputIndex)" @update:modelValue="setInput(inputIndex, $event)" :label="item.name || '(unnamed)'" :placeholder="item.type" :hint="item.type" density="compact"></v-text-field>
-                        </div>
-                        <div v-else>
-                          <v-row v-for="(arrayItem, arrayIndex) of getInput(inputIndex)" no-gutters dense>
-                            <v-col>
-                              {{ arrayIndex + 1}}
-                            </v-col>
-                            <v-col cols="9">
-                              <v-text-field :model-value="getInput(inputIndex)[arrayIndex]" @update:modelValue="setInputArrayElement(inputIndex, arrayIndex, $event)" :label="(item.name || '(unnamed)') + '[' + arrayIndex + ']'" :placeholder="item.arrayChildren.type" :hint="item.arrayChildren.type" density="compact"></v-text-field>
-                              <!-- {{ arrayItem }} -->
-                            </v-col>
-                            <v-col cols="2">
-                              <v-btn v-if="item.arrayLength == -1" @click="removeInputArrayElement(inputIndex, arrayIndex);" text class="ms-2">Delete Row</v-btn>
-                            </v-col>
-                          </v-row>
-                          <v-row no-gutters dense>
-                            <v-col cols="10">
-                            </v-col>
-                            <v-col cols="2">
-                              <v-btn v-if="item.arrayLength == -1" @click="addNewInputArrayItem(inputIndex);" text class="ms-2 mb-2">Add New Row</v-btn>
-                            </v-col>
-                          </v-row>
-                          <!-- <pre>
-array: {{ getInput(inputIndex) }}
-                          </pre> -->
-                        </div>
-                        <!-- {{ item }} -->
-                      </v-col>
-                    </v-row>
+                    <v-form ref="form">
+                      <div v-if="selectedFunctionInputs.length == 0">
+                        No inputs
+                      </div>
+                      <v-row v-for="(item, inputIndex) of selectedFunctionInputs" no-gutters dense>
+                        <v-col>
+                          {{ inputIndex + 1}}
+                        </v-col>
+                        <v-col cols="11">
+                          <div v-if="item.arrayLength == null">
+                            {{ item }}
+                            <div v-if="item.type == 'address'">
+                              <v-text-field :model-value="getInput(inputIndex)" :rules="addressRules" @update:modelValue="setInput(inputIndex, $event)" :label="item.name || '(unnamed)'" :placeholder="item.type" :hint="item.type" density="compact"></v-text-field>
+                            </div>
+                            <div v-else>
+                              <v-text-field :model-value="getInput(inputIndex)" @update:modelValue="setInput(inputIndex, $event)" :label="item.name || '(unnamed)'" :placeholder="item.type" :hint="item.type" density="compact"></v-text-field>
+                            </div>
+                          </div>
+                          <div v-else>
+                            <v-row v-for="(arrayItem, arrayIndex) of getInput(inputIndex)" no-gutters dense>
+                              <v-col>
+                                {{ arrayIndex + 1}}
+                              </v-col>
+                              <v-col cols="9">
+                                <v-text-field :model-value="getInput(inputIndex)[arrayIndex]" @update:modelValue="setInputArrayElement(inputIndex, arrayIndex, $event)" :label="(item.name || '(unnamed)') + '[' + arrayIndex + ']'" :placeholder="item.arrayChildren.type" :hint="item.arrayChildren.type" density="compact"></v-text-field>
+                                <!-- {{ arrayItem }} -->
+                              </v-col>
+                              <v-col cols="2">
+                                <v-btn v-if="item.arrayLength == -1" @click="removeInputArrayElement(inputIndex, arrayIndex);" text class="ms-2">Delete Row</v-btn>
+                              </v-col>
+                            </v-row>
+                            <v-row no-gutters dense>
+                              <v-col cols="10">
+                              </v-col>
+                              <v-col cols="2">
+                                <v-btn v-if="item.arrayLength == -1" @click="addNewInputArrayItem(inputIndex);" text class="ms-2 mb-2">Add New Row</v-btn>
+                              </v-col>
+                            </v-row>
+                            <!-- <pre>
+  array: {{ getInput(inputIndex) }}
+                            </pre> -->
+                          </div>
+                          <!-- {{ item }} -->
+                        </v-col>
+                      </v-row>
+                    </v-form>
                   </v-card-text>
                 </v-card>
-                <!-- <v-btn @click="importABIFromEtherscan();" class="ms-2 mt-0 mb-2" text>Import ABI From Etherscan -->
-                <v-btn class="ms-2 mt-2 mb-2" text>Call</v-btn>
+                <v-btn @click="callFunction();" class="ms-2 mt-2 mb-2" text>Call</v-btn>
+                <p v-if="error" class="text-error ms-2">{{ error }}</p>
                 <v-card title="Outputs" class="mt-1">
                   <v-card-text>
                     <div v-if="selectedFunctionOutputs.length == 0">
@@ -112,8 +121,19 @@ array: {{ getInput(inputIndex) }}
         outputs: {},
         version: 1,
       },
-      selected: null,
-      _timerIds: {},
+      addressRules: [
+        (v) => (v || '').length > 0 || 'Address is required',
+        (v) => {
+          try {
+            ethers.utils.getAddress(v);
+            return true;
+          } catch (e) {
+            return 'Invalid Address';
+          }
+        },
+      ],
+      error: null,
+      _timerIds: {}, // TODO: Delete
     };
   },
   computed: {
@@ -145,7 +165,7 @@ array: {{ getInput(inputIndex) }}
       },
     },
     selectedFunction() {
-      console.log(now() + " AddressContract - computed.selectedFunction");
+      // console.log(now() + " AddressContract - computed.selectedFunction");
       return this.selectedMethodId && this.functions[this.selectedMethodId] || {};
     },
     selectedFunctionInputs() {
@@ -174,6 +194,11 @@ array: {{ getInput(inputIndex) }}
       });
       return results;
     },
+    // isValid() {
+    //   // return this.$refs.form.validate();
+    //   const status = this.$refs.form && this.$refs.form.validate();
+    //   return status;
+    // },
   },
   methods: {
     getInput(index) {
@@ -254,6 +279,59 @@ array: {{ getInput(inputIndex) }}
         return new Array(this.selectedFunctionOutputs[index].arrayLength).fill(null);
       }
       return null;
+    },
+    setOutputs(outputs) {
+      console.log(now() + " AddressContract - setOutputs - outputs: " + JSON.stringify(outputs));
+      if (!(this.address in this.settings.outputs)) {
+        this.settings.outputs[this.address] = {};
+      }
+      this.settings.outputs[this.address][this.selectedMethodId] = outputs;
+      this.saveSettings();
+    },
+    async callFunction() {
+      const validationStatus = await this.$refs.form.validate();
+      console.log(now() + " AddressContract - callFunction - validationStatus: " + JSON.stringify(validationStatus, null, 2));
+      if (!validationStatus.valid) {
+        this.error = "One or more fields failed the validation check";
+      } else {
+        this.error = null;
+      }
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      // console.log(now() + " AddressContract - callFunction - selectedFunction: " + JSON.stringify(this.selectedFunction, null, 2));
+      const addressInfo = store.getters["addresses/getAddressInfo"](this.address);
+      // console.log(now() + " AddressContract - callFunction - addressInfo: " + JSON.stringify(addressInfo, null, 2));
+      const parameters = [];
+      for (const [index, input] of this.selectedFunctionInputs.entries()) {
+        console.log(index + " => " + JSON.stringify(input));
+        const value = this.getInput(index);
+        parameters.push(value);
+      }
+      // TODO
+      const contract = new ethers.Contract(this.address, addressInfo.abi, provider);
+      try {
+        const results = await contract[this.selectedFunction.name](...parameters);
+        console.log(now() + " AddressContract - callFunction - results: " + JSON.stringify(results));
+        const outputs = [];
+        for (const [index, output] of this.selectedFunction.outputs.entries()) {
+          // console.log(index + " => " + typeof output + " " + JSON.stringify(output));
+          let value;
+          if (Array.isArray(results)) {
+            value = results[index];
+          } else {
+            value = results;
+          }
+          if (output.type == "uint256") {
+            outputs.push(ethers.BigNumber.from(value).toString());
+          } else {
+            outputs.push(value);
+          }
+        }
+        console.log(now() + " AddressContract - callFunction - outputs: " + JSON.stringify(outputs));
+        this.setOutputs(outputs);
+      } catch (e) {
+        console.error(now() + " AddressContract - callFunction - ERROR: " + e.message);
+      }
     },
     saveSettings() {
       // console.log(now() + " AddressContract - saveSettings - settings: " + JSON.stringify(this.settings, null, 2));
