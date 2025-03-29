56db255f0a687a6c9328dafa7bc9253c1026e042
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-03-29 15:31:17 +1100

    address abi

--- docs/addressContract.js
+++ docs/addressContract.js
@@ -2,10 +2,15 @@ const AddressContract = {
   template: `
     <div>
       <v-card>
-        <h3 class="ms-2 mt-2">Address {{ inputAddress }} Contract</h3>
         <v-card-text>
-          <p>TODO</p>
-          <p>{{ inputAddress }}</p>
+          <div v-if="!info || info.type == 'eoa'">
+            Externally Owned Accounts don't have contract functions
+          </div>
+          <div v-if="info && info.type != 'eoa'">
+            <h3 class="ms-2 mt-2">Address {{ inputAddress }} Contract</h3>
+            <p>TODO</p>
+            <p>{{ inputAddress }}</p>
+          </div>
         </v-card-text>
       </v-card>
     </div>
@@ -13,14 +18,25 @@ const AddressContract = {
   props: ['inputAddress'],
   data: function () {
     return {
-      address: null,
+      settings: {
+        tab: null,
+        version: 0,
+      },
     };
   },
   computed: {
-
+    address() {
+      return store.getters['address/address'];
+    },
+    info() {
+      return store.getters['address/info'];
+    },
   },
   methods: {
-
+    saveSettings() {
+      console.log(now() + " AddressContract - saveSettings - settings: " + JSON.stringify(this.settings, null, 2));
+      localStorage.explorerAddressContractSettings = JSON.stringify(this.settings);
+    },
   },
   beforeCreate() {
     console.log(now() + " AddressContract - beforeCreate");
