b178fc9e6d54c8aedc844a6044442c21eb3a71af
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-04-02 13:06:49 +1100

    ENS name wip

--- docs/name.js
+++ docs/name.js
@@ -1,27 +1,20 @@
 const Name = {
   template: `
     <div>
-      <v-card>
+      <v-card v-if="!inputName">
         <v-card-text>
-          <div v-if="!inputName">
-            Enter ENS name in the search field above
-          </div>
-          <div v-else>
-            {{ inputName }}
-          </div>
-          <div v-if="timestampString">
-            timestampString: {{ timestampString }}
-          </div>
-          <v-textarea v-if="tx" :model-value="JSON.stringify(tx, null, 2)" label="Tx" rows="10">
-          </v-textarea>
-          <v-textarea v-if="txReceipt" :model-value="JSON.stringify(txReceipt, null, 2)" label="Tx Receipt" rows="10">
-          </v-textarea>
+          Enter ENS name in the search field above
         </v-card-text>
-        <!-- <v-card-actions>
-          <v-btn>Action 1</v-btn>
-          <v-btn>Action 2</v-btn>
-        </v-card-actions> -->
       </v-card>
+      <v-container v-if="inputName && name" fluid class="pa-1">
+        <v-card>
+          <v-card-text>
+            name: {{ name }}
+            <br />
+            resolvedAddress: {{ resolvedAddress }}
+          </v-card-text>
+        </v-card>
+      </v-container>
     </div>
   `,
   props: ['inputName'],
@@ -31,11 +24,11 @@ const Name = {
     };
   },
   computed: {
-    tx() {
-      return store.getters['name/tx'];
+    name() {
+      return store.getters['name/name'];
     },
-    txReceipt() {
-      return store.getters['name/txReceipt'];
+    resolvedAddress() {
+      return store.getters['name/info'].resolvedAddress;
     },
     timestamp() {
       return store.getters['name/timestamp'];
@@ -68,7 +61,7 @@ const Name = {
     console.log(now() + " Name - mounted - inputName: " + this.inputName);
     const t = this;
     setTimeout(function() {
-      store.dispatch('name/loadName', t.inputName);
+      store.dispatch('name/loadName', { inputName: t.inputName, forceUpdate: false });
     }, 1000);
 	},
   unmounted() {
@@ -77,93 +70,4 @@ const Name = {
   destroyed() {
     console.log(now() + " Name - destroyed");
 	},
-};
-
-const nameModule = {
-  namespaced: true,
-  state: {
-    error: null,
-    tx: null,
-    txReceipt: null,
-    timestamp: null,
-  },
-  getters: {
-    error: state => state.error,
-    tx: state => state.tx,
-    txReceipt: state => state.txReceipt,
-    timestamp: state => state.timestamp,
-  },
-  mutations: {
-    setData(state, data) {
-      state.error = data.error;
-      state.tx = data.tx;
-      state.txReceipt = data.txReceipt;
-      state.timestamp = data.timestamp;
-    },
-  },
-  actions: {
-    async loadName(context, { inputName, forceUpdate }) {
-      console.log(now() + " nameModule - actions.loadName - inputName: " + inputName + ", forceUpdate: " + forceUpdate);
-      let [error, tx, txReceipt, timestamp] = [null, null, null, null];
-      const txHash = false;
-      if (txHash) {
-        if (!store.getters['web3'].connected || !window.ethereum) {
-          error = "Not connected";
-        }
-        if (!error && !(/^0x([A-Fa-f0-9]{64})$/.test(txHash))) {
-          error = "Invalid transaction hash";
-        }
-        if (!error) {
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-          const tx_ = await provider.getTransaction(txHash);
-          console.log("tx_: " + JSON.stringify(tx_, null, 2));
-          tx = {
-            hash: tx_.hash,
-            chainId: parseInt(tx_.chainId),
-            blockNumber: parseInt(tx_.blockNumber),
-            transactionIndex: tx_.transactionIndex,
-            type: tx_.type || null,
-            from: tx_.from,
-            to: tx_.to,
-            nonce: parseInt(tx_.nonce),
-            value: ethers.BigNumber.from(tx_.value).toString(),
-            data: tx_.data,
-            gasLimit: parseInt(tx_.gasLimit),
-            gasPrice: ethers.BigNumber.from(tx_.gasPrice).toString(),
-            maxFeePerGas: tx_.maxFeePerGas && ethers.BigNumber.from(tx_.maxFeePerGas).toString() || null,
-            maxPriorityFeePerGas: tx_.maxPriorityFeePerGas && ethers.BigNumber.from(tx_.maxPriorityFeePerGas).toString() || null,
-            // accessList: tx_.accessList,
-            // blockHash: tx_.blockHash,
-            // r: tx_.r,
-            // s: tx_.s,
-            // v: tx_.v,
-            // creates: tx_.creates,
-          };
-          const txReceipt_ = await provider.getTransactionReceipt(txHash);
-          console.log("txReceipt_: " + JSON.stringify(txReceipt_, null, 2));
-          txReceipt = {
-            status: txReceipt_.status,
-            type: txReceipt_.type,
-            byzantium: txReceipt_.byzantium || null,
-            contractAddress: txReceipt_.contractAddress,
-            gasUsed: parseInt(txReceipt_.gasUsed),
-            cumulativeGasUsed: parseInt(txReceipt_.cumulativeGasUsed),
-            effectiveGasPrice: ethers.BigNumber.from(txReceipt_.effectiveGasPrice).toString(),
-            logs: txReceipt_.logs,
-            // to: txReceipt_.to,
-            // from: txReceipt_.from,
-            // blockNumber: parseInt(txReceipt_.blockNumber),
-            // logsBloom: txReceipt_.logsBloom,
-            // blockHash: txReceipt_.blockHash,
-          };
-          const block = tx && await provider.getBlock(tx.blockNumber) || null;
-          timestamp = block && block.timestamp || null;
-          if (!tx || !txReceipt) {
-            error = "Transaction with specified hash cannot be found"
-          }
-        }
-      }
-      context.commit('setData', { error, tx, txReceipt, timestamp });
-    }
-  },
 };
---
+++ docs/nameModule.js
@@ -0,0 +1,109 @@
+const nameModule = {
+  namespaced: true,
+  state: {
+    info: {},
+  },
+  getters: {
+    name: state => state.info.name || null,
+    info: state => state.info,
+    // functions(state) {
+    //   console.log(now() + " nameModule - computed.functions");
+    //   const addressInfo = store.getters["addresses/getAddressInfo"](state.info.address);
+    //   // console.log(now() + " nameModule - computed.functions - addressInfo: " + JSON.stringify(addressInfo));
+    //   const results = {};
+    //   if (addressInfo.abi) {
+    //     try {
+    //       const interface = new ethers.utils.Interface(addressInfo.abi);
+    //       for (const fullName of interface.format(ethers.utils.FormatTypes.full)) {
+    //         if (fullName.substring(0, 8) == "function") {
+    //           const fragment = interface.getFunction(fullName.substring(9,));
+    //           const methodId = interface.getSighash(fragment);
+    //           results[methodId] = { fullName, ...fragment };
+    //         }
+    //       }
+    //     } catch (e) {
+    //       console.error(now() + " nameModule - computed.functions - ERROR: " + e.message);
+    //     }
+    //   }
+    //   return results;
+    // },
+    // events(state) {
+    //   console.log(now() + " nameModule - computed.events");
+    //   const addressInfo = store.getters["addresses/getAddressInfo"](state.info.address);
+    //   // console.log(now() + " nameModule - computed.functions - addressInfo: " + JSON.stringify(addressInfo));
+    //   const results = {};
+    //   if (addressInfo.abi) {
+    //     try {
+    //       const interface = new ethers.utils.Interface(addressInfo.abi);
+    //       for (const fullName of interface.format(ethers.utils.FormatTypes.full)) {
+    //         if (fullName.substring(0, 5) == "event") {
+    //           const fragment = interface.getEvent(fullName.substring(6,));
+    //           const topicCount = fragment.inputs.filter(e => e.indexed).length + 1;
+    //           const signature = interface.getEventTopic(fragment);
+    //           // const parameters = fragment.inputs.map(e => ({ name: e.name, type: e.type, indexed: e.indexed }));
+    //           results[signature] = { fullName, topicCount, ...fragment };
+    //         }
+    //       }
+    //     } catch (e) {
+    //       console.error(now() + " nameModule - computed.events - ERROR: " + e.message);
+    //     }
+    //   }
+    //   return results;
+    // },
+  },
+  mutations: {
+    setInfo(state, info) {
+      console.log(now() + " nameModule - mutations.setData - info: " + JSON.stringify(info));
+      state.info = info;
+    },
+  },
+  actions: {
+    async loadName(context, { inputName, forceUpdate }) {
+      console.log(now() + " nameModule - actions.loadName - inputName: " + inputName + ", forceUpdate: " + forceUpdate);
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const chainId = store.getters["chainId"];
+      const dbInfo = store.getters["db"];
+      const info = {};
+      if (ethers.utils.isValidName(inputName)) {
+        info.name = inputName;
+        info.resolvedAddress = await provider.resolveName(inputName);
+        console.log(now() + " nameModule - actions.loadName - resolvedAddress: " + info.resolvedAddress);
+      }
+      context.commit('setInfo', info);
+      // db.close();
+    },
+    async loadAddress(context, { inputAddress, forceUpdate }) {
+      console.log(now() + " nameModule - actions.loadAddress - inputAddress: " + inputAddress + ", forceUpdate: " + forceUpdate);
+      // TODO
+      // if (!store.getters['web3'].connected || !window.ethereum) {
+      //   error = "Not connected";
+      // }
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const chainId = store.getters["chainId"];
+      const dbInfo = store.getters["db"];
+      // console.log(now() + " nameModule - actions.loadAddress - dbInfo: " + JSON.stringify(dbInfo));
+      const db = new Dexie(dbInfo.name);
+      db.version(dbInfo.version).stores(dbInfo.schemaDefinition);
+
+      const validatedAddress = validateAddress(inputAddress);
+      let info = {};
+      if (validatedAddress) {
+        info = await dbGetCachedData(db, validatedAddress + "_" + chainId + "_address", {});
+        // console.log(now() + " nameModule - actions.loadAddress - info: " + JSON.stringify(info));
+        if (Object.keys(info).length == 0 || forceUpdate) {
+          info = await getAddressInfo(validatedAddress, provider);
+          // console.log(now() + " nameModule - actions.loadAddress - info: " + JSON.stringify(info));
+          await dbSaveCacheData(db, validatedAddress + "_" + chainId + "_address", info);
+        }
+        const addressInfo = store.getters["addresses/getAddressInfo"](validatedAddress);
+        // console.log(now() + " nameModule - actions.loadAddress - addressInfo: " + JSON.stringify(addressInfo));
+        if (!addressInfo.type) {
+          store.dispatch("addresses/addAddress", { address: validatedAddress, type: info.type, version: info.version });
+        }
+      }
+
+      context.commit('setInfo', info);
+      db.close();
+    },
+  },
+};
