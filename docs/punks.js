2c81852417928d4326abee723d72acb31bd9e69f
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-05-03 08:20:43 +1000

    tidy

--- docs/punks.js
+++ docs/punks.js
@@ -144,7 +144,7 @@ const Punks = {
                         <v-card class="pb-2" :max-width="settings.tokens.view != 'medium' ? 260 : 130">
                           <v-img :src="'data:image/png;base64,' + images[item[0]]" :width="settings.tokens.view != 'medium' ? 260 : 130" cover align="left" class="align-end text-white" style="image-rendering: pixelated; background-color: #638596;">
                             <!-- <v-card-title v-if="settings.tokens.view == 'large'" class="text-left" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-size: 1em;">{{ item[0] }}</v-card-title>
-                            <!-- <v-card-title v-if="settings.tokens.view == 'medium'" class="text-left" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-size: 0.7em;">{{ item[0] }}</v-card-title> --> -->
+                            <!-- <v-card-title v-if="settings.tokens.view == 'medium'" class="text-left" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-size: 0.7em;">{{ item[0] }}</v-card-title> -->
                             <v-chip label size="x-small" variant="flat" color="secondary" class="ma-1">
                               {{ commify0(item[0]) }}
                             </v-chip>
