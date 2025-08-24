8aff78fc1689b94f38f20eab178e8ab1c2f927de
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-08-24 11:46:29 +1000

    address book wip

--- docs/addressBook.js
+++ docs/addressBook.js
@@ -2,15 +2,23 @@ const AddressBook = {
   template: `
     <div>
       <v-dialog :model-value="show" persistent theme="system" max-width="800px">
-        <v-card title="Address Book" min-height="50vh">
+        <v-card min-height="50vh">
+          <v-card-title class="ma-0 pa-1">
+            <h4 class="ml-2">Address Book</h4>
+          </v-card-title>
+          <v-toolbar density="compact">
+            <h4 class="ml-2">Blah</h4>
+          </v-toolbar>
           <!-- <v-card-text class="ma-2 pa-2"> -->
           <v-card-text>
-            Blah
+            <!-- {{ addressesList }} -->
+            <!-- {{ tagsList }} -->
+            {{ tags }}
           </v-card-text>
           <v-card-actions>
             <!-- <v-btn @click="hide();" prepend-icon="mdi-window-close" variant="text" class="lowercase-btn">Cancel</v-btn> -->
-            <v-btn @click="hide();" prepend-icon="mdi-check" size="small" class="lowercase-btn">OK</v-btn>
-            <v-btn @click="hide();" prepend-icon="mdi-window-close" size="small" class="lowercase-btn">Cancel</v-btn>
+            <v-btn @click="hide();" prepend-icon="mdi-check" class="lowercase-btn">OK</v-btn>
+            <v-btn @click="hide();" prepend-icon="mdi-window-close" class="lowercase-btn">Cancel</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -32,6 +40,36 @@ const AddressBook = {
   computed: {
     show() {
       return store.getters['addressBook/show'];
+    },
+    addresses() {
+      return store.getters['addressBook/addresses'];
+    },
+    tags() {
+      return store.getters['addressBook/tags'];
+    },
+    addressesList() {
+      const results = [];
+      for (const [address, addressData] of Object.entries(this.addresses)) {
+        // console.error(address + " => " + JSON.stringify(addressData));
+        results.push({ value: address, title: address, tags: addressData.tags });
+      }
+      return results;
+    },
+    tagsList() {
+      const collator = {};
+      const results = [];
+      for (const [address, addressData] of Object.entries(this.addresses)) {
+        console.error(address + " => " + JSON.stringify(addressData));
+        results.push({ value: address, title: address, tags: addressData.tags });
+        for (let tag of addressData.tags) {
+          console.error("  " + tag);
+          if (!(tag in collator)) {
+            collator[tag] = [];
+          }
+          collator[tag].push(address);
+        }
+      }
+      return collator;
     },
   },
   methods: {
@@ -69,12 +107,77 @@ const addressBookModule = {
   state: {
     addressBook: {
       show: false,
-      addresses: {},
-      version: 0,
+      addresses: {
+        "0x0000...0003": {
+          tags: [ "test3", "test" ],
+        },
+        "0x0000...0001": {
+          tags: [ "test1", "test" ],
+        },
+        "0x0000...0002": {
+          tags: [ "test2", "test" ],
+        },
+        "0x0000...0004": {
+          tags: [ "test4", "test" ],
+        },
+        "0x0000...0005": {
+          tags: [ "test5", "test" ],
+        },
+        "0x0000...0006": {
+          tags: [ "test6", "test" ],
+        },
+        "0x0000...0007": {
+          tags: [ "test7", "test" ],
+        },
+        "0x0000...0008": {
+          tags: [ "test8", "test" ],
+        },
+        "0x0000...0009": {
+          tags: [ "test9", "test" ],
+        },
+        "0x0000...0010": {
+          tags: [ "test10", "test" ],
+        },
+        "0x0000...0011": {
+          tags: [ "test11", "test" ],
+        },
+        "0x0000...0012": {
+          tags: [ "test12", "test" ],
+        },
+        "0x0000...0013": {
+          tags: [ "test13", "test" ],
+        },
+        "0x0000...0014": {
+          tags: [ "test14", "test" ],
+        },
+        "0x0000...0015": {
+          tags: [ "test15", "test" ],
+        },
+      },
+      version: 1,
     },
   },
   getters: {
     show: state => state.addressBook.show,
+    addresses: state => state.addressBook.addresses,
+    tags(state) {
+      console.error(now() + " addressBookModule - getters.tags");
+      const results = {};
+
+      for (const [address, addressData] of Object.entries(state.addressBook.addresses)) {
+        console.error(address + " => " + JSON.stringify(addressData));
+        // results.push({ value: address, title: address, tags: addressData.tags });
+        for (let tag of addressData.tags) {
+          // console.error("  " + tag);
+          if (!(tag in results)) {
+            results[tag] = [];
+          }
+          results[tag].push(address);
+        }
+      }
+
+      return results;
+    },
   },
   mutations: {
     setAddressBook(state, addressBook) {
