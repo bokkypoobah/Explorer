ba8ff82fa0f84309fe84e66ebd556bd7902b06a2
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-08-24 15:33:00 +1000

    address book wip

--- docs/addressBook.js
+++ docs/addressBook.js
@@ -8,6 +8,11 @@ const AddressBook = {
           </v-card-title>
           <v-toolbar density="compact">
             <h4 class="ml-2">Blah</h4>
+            <v-spacer></v-spacer>
+            <v-tabs v-model="tab" right color="deep-purple-accent-4">
+              <v-tab prepend-icon="mdi-numeric" text="Addresses" value="addresses" class="lowercase-btn"></v-tab>
+              <v-tab prepend-icon="mdi-tag" text="Tags" value="tags" class="lowercase-btn"></v-tab>
+            </v-tabs>
           </v-toolbar>
           <!-- <v-card-text class="ma-2 pa-2"> -->
           <v-card-text>
@@ -40,6 +45,14 @@ const AddressBook = {
   computed: {
     show() {
       return store.getters['addressBook/show'];
+    },
+    tab: {
+      get: function () {
+        return store.getters['addressBook/tab'];
+      },
+      set: function (tab) {
+        store.dispatch('addressBook/setTab', tab);
+      },
     },
     addresses() {
       return store.getters['addressBook/addresses'];
@@ -107,75 +120,87 @@ const addressBookModule = {
   state: {
     addressBook: {
       show: false,
+      tab: "addresses",
       addresses: {
         "0x0000...0003": {
+          name: "address3",
           tags: [ "test3", "test" ],
         },
         "0x0000...0001": {
+          name: "address1",
           tags: [ "test1", "test" ],
         },
         "0x0000...0002": {
+          name: "address2",
           tags: [ "test2", "test" ],
         },
         "0x0000...0004": {
+          name: "address4",
           tags: [ "test4", "test" ],
         },
         "0x0000...0005": {
+          name: "address5",
           tags: [ "test5", "test" ],
         },
         "0x0000...0006": {
+          name: "address6",
           tags: [ "test6", "test" ],
         },
         "0x0000...0007": {
+          name: "address7",
           tags: [ "test7", "test" ],
         },
         "0x0000...0008": {
+          name: "address8",
           tags: [ "test8", "test" ],
         },
         "0x0000...0009": {
+          name: "address9",
           tags: [ "test9", "test" ],
         },
         "0x0000...0010": {
+          name: "address10",
           tags: [ "test10", "test" ],
         },
         "0x0000...0011": {
+          name: "address11",
           tags: [ "test11", "test" ],
         },
         "0x0000...0012": {
+          name: "address12",
           tags: [ "test12", "test" ],
         },
         "0x0000...0013": {
+          name: "address13",
           tags: [ "test13", "test" ],
         },
         "0x0000...0014": {
+          name: "address14",
           tags: [ "test14", "test" ],
         },
         "0x0000...0015": {
+          name: "address15",
           tags: [ "test15", "test" ],
         },
       },
-      version: 1,
+      version: 2,
     },
   },
   getters: {
     show: state => state.addressBook.show,
+    tab: state => state.addressBook.tab,
     addresses: state => state.addressBook.addresses,
     tags(state) {
-      console.error(now() + " addressBookModule - getters.tags");
       const results = {};
-
       for (const [address, addressData] of Object.entries(state.addressBook.addresses)) {
-        console.error(address + " => " + JSON.stringify(addressData));
-        // results.push({ value: address, title: address, tags: addressData.tags });
         for (let tag of addressData.tags) {
-          // console.error("  " + tag);
           if (!(tag in results)) {
             results[tag] = [];
           }
           results[tag].push(address);
         }
       }
-
+      console.error(now() + " addressBookModule - getters.tags - results: " + JSON.stringify(results));
       return results;
     },
   },
@@ -187,6 +212,10 @@ const addressBookModule = {
     setShow(state, show) {
       // console.log(now() + " addressBookModule - mutations.setShow - show: " + show);
       state.addressBook.show = show;
+    },
+    setTab(state, tab) {
+      // console.log(now() + " addressBookModule - mutations.setTab - tab: " + tab);
+      state.addressBook.tab = tab;
     },
   },
   actions: {
@@ -209,6 +238,10 @@ const addressBookModule = {
     setShow(context, show) {
       // console.log(now() + " addressBookModule - actions.setShow - show: " + show);
       context.commit('setShow', show);
+    },
+    setTab(context, tab) {
+      // console.log(now() + " addressBookModule - actions.setTab - tab: " + tab);
+      context.commit('setTab', tab);
     },
   },
 };
